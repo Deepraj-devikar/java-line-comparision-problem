ca347e261e8f59812b5210a50baf5c31fe54a2c8
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-30 12:06:02 +0530

    ADDED: Line Comparision Problem With Welcome Message

---
+++ src/com/linecomparisionproblem/LineComparisionProblem.java
@@ -0,0 +1,10 @@
+package com.linecomparisionproblem;
+
+public class LineComparisionProblem {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Line Comparision Problem");
+
+	}
+
+}
